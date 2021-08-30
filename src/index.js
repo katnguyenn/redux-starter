fbc9eed0347b6ce531e8d582c1e5c2dc53be02ec
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-08-30 15:57:56 -0700

    subscribe to store to see changes after rerenders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import store from './store';
 
+// UI components should subscribe to the store so they get notified when the store changes (rerender in React)
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed", store.getState());
+})
+
 store.dispatch({
     type: 'bugAdded',
     payload: {
         description: 'Bug1'
     }
 });
+
+unsubscribe();
 
 store.dispatch({
     type: 'bugRemoved',
